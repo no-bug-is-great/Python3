3e7a9a81f6258469e50ad618ecafcdbfa3605314
CMacBook <mengtingc@CMacBook-Pro.local>
2018-11-24 10:34:19 -0500

    Practice 1

---
+++ Practice1.py
@@ -0,0 +1,15 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 00:48:40) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Practice
+>>> surname = input('Input your surname: ')
+Input your surname: C
+>>> firstname = input('Input your first name: ')
+Input your first name: MT
+>>> print('Your surname is: ', surname)
+Your surname is:  C
+>>> print('Your fist name is: ', firstname)
+Your fist name is:  MT
+>>> print('Your full name is: ', firstname, surname)
+Your full name is:  MT C
+>>> 
